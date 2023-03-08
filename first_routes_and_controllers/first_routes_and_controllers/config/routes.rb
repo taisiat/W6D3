63b49721c75907617ba835912b970d3d02454f2e
Taisia Karaseva <taisiat@gmail.com>
2023-03-08 15:07:26 -0800

    Initial commit + routes setup

---
+++ first_routes_and_controllers/first_routes_and_controllers/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # resources :users
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+end
