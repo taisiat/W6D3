1f2bf3b8d24ae056842e7ea573525e72efcbf352
Andrew Galicia <andrewlgalicia@gmail.com>
2023-03-09 10:08:28 -0800

    create update

--- first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -14,13 +14,29 @@ class UsersController < ApplicationController
     end
 
     def show
-        
-        render json: params
+        incoming_wildcard = params[:id]
+        answer = User.find_by(id:incoming_wildcard)
+        render json: answer
     end
 
     def update
+        incoming_wildcard = params[:id]
+        located_user = User.find_by(id:incoming_wildcard)
+        if !located_user 
+            render json: "User not found"
+        elsif !located_user.update!(user_params)
+            render json: located_user.errors.full_messages, status: :unprocessable_entity 
+        else
+            redirect_to user_url(located_user.id)   
+        end
     end
 
     def destroy
+    end
+
+
+    private 
+    def user_params
+        params.require(:user).permit(:name,:email)
     end
 end
