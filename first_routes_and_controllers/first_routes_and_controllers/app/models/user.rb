738bd2fdcc5c6fdd9e3a454de18260dbdf4d48bb
Andrew Galicia <andrewlgalicia@gmail.com>
2023-03-08 17:03:58 -0800

    phase 3 pt.1

---
+++ first_routes_and_controllers/first_routes_and_controllers/app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    validates :name, :email, presence: true
+    # def initialize(name, email)
+    #     @name = name
+    #     @email = email
+
+    # end
+end
