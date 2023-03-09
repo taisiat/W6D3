4fb365b511e1f538cab73c1005f0e6f6f51ae593
Taisia Karaseva <taisiat@gmail.com>
2023-03-08 17:31:42 -0800

    Add solution through create method

--- first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -5,10 +5,22 @@ class UsersController < ApplicationController
     end 
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
+        
         render json: params
+    end
+
+    def update
+    end
+
+    def destroy
     end
 end
